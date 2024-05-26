f0faa60cf0d8ce21be28a339812c102a8c7ee570
Gal Benmaman <benmaman@post.bgu.ac.il>
2024-05-26 13:22:30 +0300

    update function L_layer_model

--- main.py
+++ main.py
@@ -30,24 +30,24 @@ batch_size=512
 params,costs=L_layer_model(X_train, y_train, layer_dims, learning_rate, num_iterations, batch_size, use_batchnorm=False)
 Predict(X_test,y_test,params)
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-df=pd.DataFrame({'cost':costs})
-df["iteration"]=df.index*100
-# Create scatter plot
-plt.figure(figsize=(10, 6))
-sns.scatterplot(data=df, x='iteration', y='cost', color='blue')  # Scatter plot of points
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# df=pd.DataFrame({'cost':costs})
+# df["iteration"]=df.index*100
+# # Create scatter plot
+# plt.figure(figsize=(10, 6))
+# sns.scatterplot(data=df, x='iteration', y='cost', color='blue')  # Scatter plot of points
 
-# Connect the points with a line
-plt.plot(df['iteration'], df['cost'], 'r-')  # 'r-' specifies a red solid line
+# # Connect the points with a line
+# plt.plot(df['iteration'], df['cost'], 'r-')  # 'r-' specifies a red solid line
 
-# Set labels
-plt.xlabel('Iteration Number',size=15)
-plt.ylabel('Cost Value',size=15)
-plt.xticks(size=14)
-plt.yticks(size=14)
+# # Set labels
+# plt.xlabel('Iteration Number',size=15)
+# plt.ylabel('Cost Value',size=15)
+# plt.xticks(size=14)
+# plt.yticks(size=14)
 
-plt.savefig('plots/plot_cost_vs_iteration.png', dpi=300)  # Saves the figure to a PNG file with 300 DPI
+# plt.savefig('plots/plot_cost_vs_iteration.png', dpi=300)  # Saves the figure to a PNG file with 300 DPI
 
-plt.show()
+# plt.show()
